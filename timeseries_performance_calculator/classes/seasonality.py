98ad8fccaeba2291ed65873ff8c9db64947b9fd7
JuneYoung <nietsnie1@gmail.com>
2025-07-30 13:41:14 +0900

    fix: resolve bugs and add benchmark convenience feature

--- timeseries_performance_calculator/classes/seasonality.py
+++ timeseries_performance_calculator/classes/seasonality.py
@@ -1,17 +1,35 @@
-# from fund_insight_engine import get_timeseries_price
+from functools import reduce
+from functools import cached_property
+from universal_timeseries_transformer import extend_timeseries_by_all_dates, transform_timeseries
 from .performance import Performance
 
 class Seasonality:
     def __init__(self, timeseries, benchmark_timeseries):
         self.timeseries = timeseries
         self.benchmark_timeseries = benchmark_timeseries
-        self.perf = Performance(timeseries=timeseries, benchmark_timeseries=benchmark_timeseries)
+        self.perf = Performance(timeseries=self.prices, benchmark_index=-1)
+        self.index_name = self.timeseries.columns[0]
+        self.benchmark_name = self.benchmark_timeseries.columns[0]
 
-    def get_seasonality(self, index_name):
-        return self.perf.get_seasonality(index_name)
+    @cached_property
+    def prices(self):
+        timeseries_extended = extend_timeseries_by_all_dates(transform_timeseries(self.timeseries, option_type='str'))
+        lst_of_prices = [timeseries_extended, self.benchmark_timeseries]
+        prices = reduce(lambda x, y: x.join(y, how='left'), lst_of_prices)
+        prices = prices.ffill()
+        return prices
+
+    @cached_property
+    def seasonality(self):
+        return self.perf.get_seasonality(self.index_name)
     
-    def get_relative_seasonality(self, index_name):
-        return self.perf.get_relative_seasonality(index_name)
+    @cached_property
+    def benchmark_seasonality(self):
+        return self.perf.get_seasonality(self.benchmark_name)
+    
+    @cached_property
+    def relative_seasonality(self):
+        return self.perf.get_relative_seasonality(self.index_name)
     
 
 # class Seasonality:
